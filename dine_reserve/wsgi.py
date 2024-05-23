c155397055ce05bbcc210f7ade146705270c32d6
Dede <specialized8393@gmail.com>
2024-05-23 14:36:50 +0100

    Reformatted code using black

--- dine_reserve/wsgi.py
+++ dine_reserve/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dine_reserve.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dine_reserve.settings")
 
 application = get_wsgi_application()
